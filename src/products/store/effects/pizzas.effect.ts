8c34fdc6aa6244fbc519117e95c9a7a5f982421a
Jordan Epps <jordan.k.epps@gmail.com>
2018-03-01 18:42:39 -0800

    wip

---
+++ src/products/store/effects/pizzas.effect.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+
+import { Effect, Actions } from '@ngrx/effects';
+import { switchMap, map, catchError } from 'rxjs/operators';
+import { of } from 'rxjs/observable/of';
+
+import * as pizzaActions from '../actions/pizzas.action';
+import * as fromServices from '../../services';
+
+
+@Injectable()
+export class PizzasEffects {
+  constructor(
+    private actions$: Actions,
+    private pizzaService: fromServices.PizzasService
+  ) {}
+
+  @Effect()
+  loadPizzas$ = this.actions$.ofType(pizzaActions.LOAD_PIZZAS).pipe(
+    switchMap(() => (
+      this.pizzaService
+        .getPizzas()
+        .pipe(
+          map(pizzas => new pizzaActions.LoadPizzasSuccess(pizzas)),
+          catchError((error: any) => of(new pizzaActions.LoadPizzasFail(error)))
+        )
+    ))
+  );
+}
